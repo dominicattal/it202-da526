a31d695db29c8c3a3d69670848de0336a878e761
da526 <da526@njit.edu>
2024-09-26 20:52:47 -0400

    problem 1 completed

--- hw2/problem1.php
+++ hw2/problem1.php
@@ -9,6 +9,11 @@ function processArray($arr) {
     //start edits
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+    foreach ($arr as $val) {
+        if ($val % 2 == 1)
+            echo $val . ' ';
+    }
+    unset($val);
     //end edits
 }
 echo "Problem 1: Odd Output<br>";
