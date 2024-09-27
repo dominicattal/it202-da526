8ec9fba533fad42963a45f3730a67c75d917bb2f
da526 <da526@njit.edu>
2024-09-26 21:28:40 -0400

    Finished homework

--- hw2/problem3.php
+++ hw2/problem3.php
@@ -11,7 +11,13 @@ function bePositive($arr) {
     //start edits
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO Take each value of the $arr, convert it to positive, and set it to the same index in the $output array but with the original data type (i.e., if the source was a string the output slot value should be a string)
-    
+    // === da526 ===
+    foreach ($arr as $value) {
+        $new_val = abs($value);
+        settype($new_val, gettype($value));
+        $output[] = $new_val;
+    }
+        
     //end edits
     
     //displays the output along with their types
