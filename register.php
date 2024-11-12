3cf95b9a69377fb74ff3c7ffcd1d9d816b27b9e4
da526 <da526@njit.edu>
2024-11-11 21:12:30 -0500

    profile

--- register.php
+++ register.php
@@ -7,6 +7,10 @@ require(__DIR__ . "/partials/nav.php");
         <input type="email" name="email" required />
     </div>
     <div>
+        <label for="user">Username</label>
+        <input type="text" name="user" required />
+    </div>
+    <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
@@ -28,6 +32,7 @@ require(__DIR__ . "/partials/nav.php");
 //TODO 2: add PHP Code
 if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
     $email = se($_POST, "email", "", false);
+    $username = se($_POST, "user", "", false);
     $password = se($_POST, "password", "", false);
     $confirm = se(
         $_POST,
@@ -52,6 +57,10 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         echo "password must not be empty";
         $hasError = true;
     }
+    if (empty($username)) {
+        echo "username must not be empty";
+        $hasError = true;
+    }
     if (empty($confirm)) {
         echo "Confirm password must not be empty";
         $hasError = true;
@@ -71,9 +80,9 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         //TODO 4
         $hash = password_hash($password, PASSWORD_BCRYPT);
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
+        $stmt = $db->prepare("INSERT INTO Users (email, password, username) VALUES (:email, :password, :username)");
         try {
-            $stmt->execute([":email" => $email, ":password" => $hash]);
+            $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
             echo "Successfully registered!";
         } catch (Exception $e) {
             echo "There was a problem registering";
